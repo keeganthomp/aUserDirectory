fe118128feae399af3063dd74154d038e24fd916
keeganthomp <whereiskeegan@gmail.com>
2017-06-20 14:52:28 -0500

    adding initial files for user directory

---
+++ app.js
@@ -0,0 +1,17 @@
+var datafile = require('./data.js');
+const mustache = require('mustache-express');
+const express = require('express');
+const app = express();
+const port = 3050;
+
+
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+app.set('views', './views');
+
+app.listen(port, function(req, res){
+    console.log("Im running on port", port);
+});
+
+app.g
+
