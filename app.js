0a547d7162e405f6c6cd210af97b803295a73d47
keeganthomp <whereiskeegan@gmail.com>
2017-06-20 19:23:01 -0500

    updating dynamic renering of a user via ID

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-var datafile = require('./data.js');
-const mustache = require('mustache-express');
-const express = require('express');
+var datafile = require("./data.js");
+const mustache = require("mustache-express");
+const express = require("express");
 const app = express();
 const port = 3050;
 
+app.engine("mustache", mustache());
+app.set("view engine", "mustache");
+app.set("views", "./views");
 
-app.engine('mustache', mustache());
-app.set('view engine', 'mustache');
-app.set('views', './views');
-
-app.listen(port, function(req, res){
-    console.log("Im running on port", port);
+app.listen(port, function(req, res) {
+  console.log("Im running on port", port);
 });
 
-app.get("/",  function(req, res){
-    res.render("index", {datafile: datafile.users});
+app.get("/", function(req, res) {
+  res.render("index", { datafile: datafile.users });
 });
-
 
 app.use("/", express.static("./views"));
+
+app.get("/users/:id", function(req, res) {
+  var profiles = document.querySelectorAll(".profileWrapper");
+  for (i = 0; i < profiles.length; i++) {
+    profiles[i].addEventListener("click", function() {
+      var userId = datafile.users[i][req.params.id];
+      res.render("details", { dataUser: userId });
+    });
+  }
+});
+
+app.use("/users/:id", express.static("./views"));
--- views/data.js
+++ views/data.js
@@ -1,12 +1,1 @@
 
-var profiles = [];
-console.log(datafile);
-
-function getUserId(){
-    profiles = document.querySelector(".profileWrapper");
-    for (i = 0; i < profiles.length; i++){
-        profile[i].addEventListener("click", function(){
-
-        })
-    }
-}
